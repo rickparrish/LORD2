a1f5d924a5e702b7a8b59fe74f202774922ad372
Rick Parrish <github@rick.parrish>
2013-08-15 11:07:43 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LORD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
